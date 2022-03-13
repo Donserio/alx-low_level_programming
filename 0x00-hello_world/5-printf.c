ba57386ad1258cd9520d1f989fdfe3f7738a128c
Donserio <donserio4real.com>
2022-03-12 19:55:01 -0800

    Use printf to print string to stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
